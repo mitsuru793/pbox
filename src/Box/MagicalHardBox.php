edc34bc74c7c6f465b30550a6e611f8bf18fba91
mitsuru793 <mitsuru793@gmail.com>
2017-10-09 04:19:16 +0900

    add MagicalAccessorTrait from MagicalHardBox

--- src/Box/MagicalHardBox.php
+++ src/Box/MagicalHardBox.php
@@ -2,44 +2,9 @@
 
 namespace Pbox\Box;
 
-use OutOfRangeException;
-
 class MagicalHardBox extends Box
 {
-    public function __get(string $name)
-    {
-        if (array_key_exists($name, $this->attributes())) {
-            $getter = "get$name";
-            $value = $this->$name;
-            return method_exists($this, $getter) ? $this->$getter($value) : $value;
-        }
-        if (property_exists($this, $name)) {
-            throw new OutOfRangeException("Cannot access static property: $name");
-        }
-        throw new OutOfRangeException("Undefined property: $name");
-    }
-
-    public function __set(string $name, $value)
-    {
-        if (array_key_exists($name, $this->attributes())) {
-            $setter = "set$name";
-            if (method_exists($this, $setter)) {
-                $this->$setter($value);
-            } else {
-                $this->$name = $value;
-            }
-            return;
-        }
-        if (property_exists($this, $name)) {
-            throw new OutOfRangeException("Cannot access static property: $name");
-        }
-        throw new OutOfRangeException("Undefined property: $name");
-    }
-
-    public function __isset($name): bool
-    {
-        return isset($this->$name);
-    }
+    use MagicalAccessorTrait;
 
     public function attributes(): array
     {
