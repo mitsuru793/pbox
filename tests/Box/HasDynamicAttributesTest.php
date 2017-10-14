bedcc4bfc05b30ac3fb6d0c4b9f30c83e5454127
mitsuru793 <mitsuru793@gmail.com>
2017-10-14 16:13:56 +0900

    add HasDynamicAttributesTest.php

---
+++ tests/Box/HasDynamicAttributesTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Pbox\Box;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+class HasDynamicAttributesTest extends TestCase
+{
+    public function testAttributesReturnsProperty()
+    {
+        $attributes = [
+            'prop1' => 'value1',
+            'prop2' => 'value2',
+        ];
+
+        $double = m::mock(HasDynamicAttributes::class);
+        $this->setProp($double, 'attributes', $attributes);
+        $this->assertSame($attributes, $double->attributes());
+    }
+
+    private function setProp($object, string $prop, $value)
+    {
+        $reflectionObject = new ReflectionClass($object);
+        $reflectionProperty = $reflectionObject->getProperty($prop);
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($object, $value);
+    }
+}
