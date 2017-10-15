2adac71280aa75c1b324c9d0e0f6647e4b0beec0
mitsuru793 <mitsuru793@gmail.com>
2017-10-15 17:44:30 +0900

    add HasDynamicPropertyOfArray.php and its test

---
+++ src/Box/HasDynamicPropertyOfArray.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Pbox\Box;
+
+use OutOfRangeException;
+
+/**
+ * Trait HasDynamicPropertyOfArray
+ *
+ * Must define property $attributes as array
+ * protected/private $attributes = ['name' => 'initial value']
+ *
+ * @package Pbox\Box
+ */
+trait HasDynamicPropertyOfArray
+{
+    public function __get(string $name)
+    {
+        if (array_key_exists($name, $this->attributes)) {
+            $getter = "get{$name}Attribute";
+            $value = $this->attributes[$name];
+            return method_exists($this, $getter) ? $this->$getter($value) : $value;
+        }
+        if (property_exists($this, $name)) {
+            throw new OutOfRangeException("Cannot access static property: $name");
+        }
+        throw new OutOfRangeException("Undefined property: $name");
+    }
+
+    public function __set(string $name, $value)
+    {
+        if (array_key_exists($name, $this->attributes)) {
+            $setter = "set{$name}Attribute";
+            if (method_exists($this, $setter)) {
+                $this->$setter($value);
+            } else {
+                $this->attributes[$name] = $value;
+            }
+            return;
+        }
+        if (property_exists($this, $name)) {
+            throw new OutOfRangeException("Cannot access static property: $name");
+        }
+        throw new OutOfRangeException("Undefined property: $name");
+    }
+
+    public function __isset($name): bool
+    {
+        return isset($this->attributes[$name]);
+    }
+
+    public function attributes(): array
+    {
+        return $this->attributes;
+    }
+}
---
+++ tests/Box/HasDynamicPropertyOfArrayTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Pbox\Box;
+
+use PHPUnit\Framework\TestCase;
+
+class HasDynamicPropertyOfArrayTest extends TestCase
+{
+    public function testSetAndGetPropertyWithoutAccessor()
+    {
+        $mock = new class
+        {
+            use HasDynamicPropertyOfArray;
+            private $attributes = ['prop' => null];
+        };
+        $mock->prop = 'from outside';
+        $this->assertSame('from outside', $mock->prop);
+    }
+
+    public function testCallSetterWhenSetProperty()
+    {
+        $mock = new class
+        {
+            use HasDynamicPropertyOfArray;
+            private $attributes = ['prop' => null];
+
+            public function setPropAttribute($value)
+            {
+                $this->prop = 'from setter';
+            }
+        };
+        $mock->prop = 'from outside';
+        $this->assertSame('from setter', $mock->prop);
+    }
+
+    public function testCallGetterWhenSetProperty()
+    {
+        $mock = new class
+        {
+            use HasDynamicPropertyOfArray;
+            private $attributes = ['prop' => null];
+
+            public function getPropAttribute($value)
+            {
+                return 'from setter';
+            }
+        };
+        $mock->prop = 'from outside';
+        $this->assertSame('from setter', $mock->prop);
+    }
+
+    public function testNotCallAccessorWhenAccessPublicProperty()
+    {
+        $mock = new class
+        {
+            use HasDynamicPropertyOfArray;
+            public $prop;
+
+            public function getPropAttribute($value)
+            {
+                return 'from getter';
+            }
+
+            public function setPropAttribute($value)
+            {
+                return 'from setter';
+            }
+        };
+        $mock->prop = 'from outside';
+        $this->assertSame('from outside', $mock->prop);
+    }
+
+    public function testAttributesGetter()
+    {
+        $mock = new class
+        {
+            use HasDynamicPropertyOfArray;
+            private $attributes = ['p1' => 'v1', 'p2' => 'v2'];
+        };
+        $expected = ['p1' => 'v1', 'p2' => 'v2'];
+        $this->assertSame($expected, $mock->attributes());
+    }
+
+    public function testIsset()
+    {
+        $mock = new class
+        {
+            use HasDynamicPropertyOfArray;
+            private $attributes = ['p1' => 'v1', 'p2' => null];
+        };
+        $this->assertTrue(isset($mock->p1));
+        $this->assertFalse(isset($mock->p2));
+        $this->assertFalse(isset($mock->invalid));
+    }
+}
