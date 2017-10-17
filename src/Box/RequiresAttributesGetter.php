1961855ce8d607b5ef3f8027bda9904974d6e6c5
mitsuru793 <mitsuru793@gmail.com>
2017-10-17 23:23:43 +0900

    add PHPDoc to a trait RequiresAttributesGetter

--- src/Box/RequiresAttributesGetter.php
+++ src/Box/RequiresAttributesGetter.php
@@ -5,13 +5,19 @@ namespace Pbox\Box;
 trait RequiresAttributesGetter
 {
     /**
-     * @param $name
-     * @return mixed
+     * Returns an exposed property to outside.
+     * You can expose a property to outside even if they are not public, so protected or private.
+     *
+     * @param string $name attribute name
+     * @return mixed attribute
      */
     abstract function attribute(string $name);
 
     /**
-     * @return array of attributes
+     * Returns array of exposed properties to outside.
+     * You can expose properties to outside even if they are not public, so protected or private.
+     *
+     * @return array attributes
      */
     abstract function attributes(): array;
 
